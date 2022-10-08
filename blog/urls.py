6d2dad66a0bd92f015cb9ad175c28c4ab0e27cdb
runydek <3dhiykurniawan@gmail.com>
2022-10-08 22:04:49 +0700

    learn fk & manytomany field

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import blog_list, BlogDetail, user_blog, blog_tags
+from .views import blog_list, BlogDetail, user_blog, blog_tags, detail_blog_author
 
 
 urlpatterns = [
     path('', blog_list, name="home"),
     path('<str:slug>/', BlogDetail.as_view(), name='blog_detail'),
-    path('blog/author/<slug:author>/', user_blog, name='user_blog'),
-    path('blog/tags/<slug:tags>/', blog_tags, name="tags")
+    path('blog/author/<str:author>/', user_blog, name='user_blog'),
+    path('blog/tags/<str:tags>/', blog_tags, name="tags"),
+    path('blog/author/<str:author>/blog/<str:slug>/', detail_blog_author, name="detail_blog_author")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
 
-from .models import Blog, Tag
+from .models import Blog, Tag, User
 
 
 def blog_list(request):
@@ -39,3 +39,12 @@ def blog_tags(request, tags):
     }
 
     return render(request, 'tag.html', context)
+
+
+def detail_blog_author(request, author, slug):
+    detail = Blog.objects.get(author__username=author, slug=slug)
+    context = {
+        'detail': detail
+    }
+
+    return render(request, 'detail_user_post.html', context)
